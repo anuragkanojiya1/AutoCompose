412287eebcaf98256e98437c782534e4bb468710
Anurag Kanojiya <anurag.kanojiya829@gmail.com>
2025-04-21 00:20:19 +0530

    Updated Api

--- app/src/main/java/com/example/autocompose/data/api/Api.kt
+++ app/src/main/java/com/example/autocompose/data/api/Api.kt
@@ -1,9 +1,11 @@
 package com.example.autocompose.data.api
 
+import com.example.autocompose.domain.model.AnalyticsResponse
 import com.example.autocompose.domain.model.BackendResponse
 import com.example.autocompose.domain.model.Model
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.POST
 
@@ -14,4 +16,8 @@ interface Api {
     fun apiCall(
         @Body request: Model
     ) : Call<BackendResponse>
+
+    @GET("/trending")
+    @Headers("Content-Type: application/json")
+    fun getTrending() : Call<AnalyticsResponse>
 }
