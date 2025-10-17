258aa5fefafe61e42b78bad0b2f734f73739c91e
Anurag Kanojiya <anurag.kanojiya829@gmail.com>
2025-10-18 00:42:39 +0530

    chore: updated some dependencies version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    id("com.google.devtools.ksp") version "2.2.20-2.0.4" apply false
+    id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
     alias(libs.plugins.google.gms.google.services) apply false
 
 }
