57c110b702b80f961d0bb949c79ac3642a6e99f7
Anurag Kanojiya <anurag.kanojiya829@gmail.com>
2025-11-16 17:31:44 +0530

    chore: updated BottomBar.kt and HomeScreen.kt

--- app/src/main/java/com/example/autocompose/ui/components/BottomBar.kt
+++ app/src/main/java/com/example/autocompose/ui/components/BottomBar.kt
@@ -1,4 +1,4 @@
-package com.example.autocompose.ui.composables
+package com.example.autocompose.ui.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
