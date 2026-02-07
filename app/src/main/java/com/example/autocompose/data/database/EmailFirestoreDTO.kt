0b015191f9538062567df448bba6a64762d66ed6
Anurag Kanojiya <anurag.kanojiya829@gmail.com>
2026-02-07 18:13:51 +0530

    chore: fixing bug

--- app/src/main/java/com/example/autocompose/data/database/EmailFirestoreDTO.kt
+++ app/src/main/java/com/example/autocompose/data/database/EmailFirestoreDTO.kt
@@ -14,5 +14,4 @@ data class EmailRoomDto(
     val subject: String = "",
     val emailBody: String = "",
     val frequency: Int = 1,
-    val updatedAt: FieldValue = FieldValue.serverTimestamp()
 )
